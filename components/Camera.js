dab04967e990fcf2b796ac8ec1c766099c8b4386
Will Lutz <lutz@njit.edu>
2020-10-04 20:59:03 -0400

    works

--- components/Camera.js
+++ components/Camera.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Alert, ActivityIndicator } from 'react-native';
+import { Text, View, ActivityIndicator, StyleSheet, Modal, TouchableHighlight } from 'react-native';
 import { Camera } from 'expo-camera';
 import CaptureButton from './CaptureButton';
+import identifyImage from '../apis/identifyImage';
 
 export default () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [loading, setLoading] = useState(true);
   const [identifiedImg, setIdentifiedImg] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -24,15 +26,22 @@ export default () => {
   }
   const snap = async () => {
     if (this.camera) {
-      this.camera.pausePreview(); // v3
       setLoading(true);
 
-      let photo = await this.camera.takePictureAsync({base64: true}); // v3
-      let { base64 } = photo
-      identifyImage(base64, displayAnswer); // v3
-      // Alert.alert(uri) // v3 remove
-      Alert.alert(identifiedImg,'',{ cancelable: false }); //v3
-      this.camera.resumePreview(); // v3
+      this.camera.takePictureAsync({base64: true})
+      .then( photo => {
+        this.camera.pausePreview(); // v3
+        let { base64 } = photo;
+
+        identifyImage(base64, displayAnswer); // v3
+        setModalVisible(true);
+        this.camera.resumePreview(); // v3
+      })
+      .catch(e => {
+        console.log('That did not go well.')
+        setModalVisible(false)
+        throw e
+      }); // v3
     }
   };
 
@@ -42,30 +51,89 @@ export default () => {
   }
 
   return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={Camera.Constants.Type.back} ref={ref => {
-        this.camera = ref;
+    <View style={styles.fullFlex}>
+      <Camera 
+        style={styles.fullFlex} 
+        type={Camera.Constants.Type.back} 
+        ref={ref => {
+          this.camera = ref;
       }}>
         <ActivityIndicator 
           size="large" 
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }} 
+          style={styles.centered} 
           color="#fff" 
           animating={loading}
           />
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-          {/* Below added for step 2 */}
-        </View>
           <CaptureButton buttonDisabled={loading} onClick={snap}/>
       </Camera>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>{identifiedImg ? identifiedImg : "Can't ID this one!"}</Text>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.textStyle}>ID More</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  openButton: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  fullFlex: {
+    flex: 1
+  },
+  centered: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
